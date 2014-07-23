11c31a9a342fdf3bec27627dc005e6dfc9a4f3d2
Ryan McCormack <writepython@gmail.com>
2014-07-22 23:24:25 -0400

    Added requests library - Significant progress

--- regex_test.py
+++ regex_test.py
@@ -4,9 +4,12 @@ tests = ["", " ", "2014", "/2014/07", "/2014/0711", "/2014/0711/", "/2014-07/11/
 
 regex1 = re.compile(r"/2014/07../")
 regex2 = re.compile(r"/2014-07/../")
+regex3 = re.compile(r"/2014-07/\d\d/")
 
 for test in tests:
     if regex1.search(test):
         print "regex1 found:%s" % test
     if regex2.search(test):
         print "regex2 found:%s" % test
+    if regex3.search(test):
+        print "regex3 found:%s" % test        
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os, errno
-import urllib, urlparse
+#import urllib
+import urlparse
+import requests
 from bs4 import BeautifulSoup
 
-from config import urls_to_crawl
+from config import urls_to_crawl, content_types_to_download
 
 def mkdir_p(path):
     try:
@@ -11,44 +13,76 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
-            
-def url_should_be_downloaded(url):
-    return True
 
-def download_url(url):
+def conditionally_download_file(current_url, html=None):            
     return True
 
 def add_new_urls(current_url, html):
     parsed_html = BeautifulSoup(html)
     for tag in parsed_html.findAll('a', href=True):
         href_absolute_url = urlparse.urljoin(current_url, tag['href'])
-        if current_url in href_absolute_url and href_absolute_url not in all_urls:
+        if follow_links_containing in href_absolute_url and href_absolute_url not in all_urls:
             urls_to_visit.append(href_absolute_url)
             all_urls.append(href_absolute_url)
 
-def crawl_url(url):
-    print "*** NEW CRAWLING SESSION FOR URL: %s ***" % url
+def crawl_url():
+    print "*** NEW CRAWLING SESSION FOR URL: %s ***" % initial_url
 
     while len(urls_to_visit) > 0:
         current_url = urls_to_visit.pop(0)
-        urls_visited.append(current_url)
-        print "Requesting url: ", current_url
+        print len(urls_to_visit)
+        head_response = requests.head(current_url)
+        if head_response.status_code == requests.codes.ok:
+            current_url_head_content_type = head_response.headers.get('content-type')
+            if current_url_head_content_type == 'text/html':
+                get_response = requests.get(current_url)
+                if get_response.status_code == requests.codes.ok:
+                    html = get_response.text
+                    add_new_urls(current_url, html)
+                    if 'text/html' in content_types_to_download:
+                        conditionally_download_file(current_url, html)
+            else:
+                
+                    
+
+                    
+                            r.status_code == requests.codes.ok
+                        GET and parse
+
+            potentially download
+        else:
+            potentially download
+            
+
+            r.status_code == requests.codes.ok
+
+
         try:
             html = urllib.urlopen(current_url).read()
-            try:
-                add_new_urls(current_url, html)
-            except:
-                print "Error parsing html for url: ", current_url          
         except:
             print "Error accessing url: ", current_url
+        else:
+            try:
 
+            except:
+                print "Error parsing html for url: ", current_url
+            else:
+                try:
+                    conditionally_download_url(current_url, html)
+                except:
+                    print "Error downloading file: ", current_url
+if regex1.search(test)
                 
 if __name__ == "__main__":
     #output_directory = os.path.join( os.path.dirname(__file__), 'output' )
-    print os.getcwd()
+    output_directory = os.path.join( os.getcwd(), "output" )
 
-    for url in urls_to_crawl:
-        initial_url = url
-        urls_to_visit = [url]
-        all_urls = [url]    
-        crawl_url(url)
+    for d in urls_to_crawl:
+        initial_url = d["url"]
+        urls_to_visit = [initial_url]
+        all_urls = [initial_url]
+        follow_link_containing = d["follow_links_containing"]
+        regex_filters = [ re.compile(regex_filter) for regex_filter in d["regex_filters"] ]
+        if regex_filters:
+            using_regex_filters = True                          
+        crawl_url()
