e12ddb8d343d8721170a3ccee3a97343a4506086
Ryan McCormack <writepython@gmail.com>
2014-07-24 13:50:58 -0400

    Working on turning URL to path

--- paths_tests.py
+++ paths_tests.py
@@ -1,9 +1,10 @@
 import os, urlparse
 
+file_extensions_to_download = ['.html', '.htm']
 unacceptable_directory_names = [ ".", "..", "/" ]
 acceptable_path_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._'
 
-urls = ["http://www.google.com", "http://www.google.com/", "http://www.google.com/a/", "http://www.google.com/a"]
+urls = ["http://www.google.com", "http://www.google.com/", "http://www.google.com/a/", "http://www.google.com/a", ]
 
 output_dir = os.path.join( os.getcwd(), 'output' )
 if not os.path.exists(output_dir):
@@ -12,12 +13,16 @@ if not os.path.exists(output_dir):
 for url in urls:
     url_parsed = urlparse.urlparse(url)
     url_netloc = url_parsed.netloc
-    url_path = url_parsed.path.strip("/")
-    dir_path_sections = [ url_netloc ]
-    for p in url_path.split('/'):
-        dir_path_sections.append(p)
+    url_path = url_parsed.path.strip().strip("/")
+    if url_path and "/" in url_path:    
+        url_path_split = url_path.split('/')
+        
+        for extension in file_extensions_to_download:
+            if 
+
+    dir_path_sections.append(p)
     netloc_dir = os.path.join( output_dir, url_parsed.netloc)
     if not os.path.exists(netloc_dir):
         mkdir_p(netoc_dir)
 
-        # If no file extension, add GUID and treat as file
+        # If no file extension, add '.file' extension and treat as file
