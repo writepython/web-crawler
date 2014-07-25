b0487c64435b8fae671564b58bb0b4febf067ea0
Ryan McCormack <writepython@gmail.com>
2014-07-25 02:15:51 -0400

    Currently in Testing mode

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-import os, errno, string, urlparse, mimetypes
+import os, re, sys, errno, traceback, string, urlparse, mimetypes
 import requests
 from bs4 import BeautifulSoup
 
-from config import urls_to_crawl, file_extensions_list, mimetypes_list
+from config import urls_to_crawl, file_extensions_list, mimetypes_list, request_timeout
 
 fs_path_acceptabl_chars = frozenset('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/._')
+files_written = 0            
     
 def mkdir_p(path):
     try:
@@ -14,15 +15,17 @@ def mkdir_p(path):
             pass
         else: raise
 
-def download_file(current_url, data):
+def download_file(current_url, data, encoding):
     """
     Data will either be data from an HTML file or None
+    Encoding is the encoding of the data
     """
-    print "Attempting to download file: ", current_url
+    print "Attempting to download file"
     if not data:
-        get_response = requests.get(current_url)
+        get_response = requests.get(current_url, timeout=request_timeout)
         if get_response.status_code == requests.codes.ok:
             data = get_response.text
+            encoding = get_response.encoding
         else:
             print "Could not get data from file: ", current_url
     if data:
@@ -30,25 +33,27 @@ def download_file(current_url, data):
         netloc = url_parsed.netloc
         url_path = url_parsed.path.strip().lstrip("/")
         if url_path == "":
-            filename = "root.file"
+            filename = "root.file" # Could guess extension based on mimetype
             fs_path = netloc
         else:
-            if url_path.endswith("/"): # Still not catching the edge case where we have file /abc and then later directory /abc/
-                url_path = url_path + "root.file"
+            if url_path.endswith("/"): # There is still an extreme edge case where we have file /abc and then later directory /abc/
+                url_path = url_path + "root.file" # Could guess extension based on mimetype
             url_path = netloc + "/" + url_path
             url_path = ''.join(c for c in url_path if c in fs_path_acceptabl_chars)                
             url_path_list = url_path.split("/")
             filename = url_path_list.pop()
             fs_path = "/".join(url_path_list)            
         fs_path = os.path.join( output_dir, fs_path)
-        print fs_path
         mkdir_p(fs_path)
         filepath = os.path.join(fs_path, filename)
+        print "* Writing file: ", filepath
         with open(filepath, 'w') as f:
-            f.write(data.encode('utf8'))            
+            f.write( data.encode(encoding) )
+        global  files_written
+        files_written += 1        
         
 def add_new_urls(current_url, html):
-    print "Adding new links found at: ", current_url
+    print "Adding new links"
     parsed_html = BeautifulSoup(html)
     for tag in parsed_html.findAll('a', href=True):
         href_absolute_url = urlparse.urljoin(current_url, tag['href'].strip() ) # Stripping handles <a href=" http...
@@ -57,47 +62,56 @@ def add_new_urls(current_url, html):
             all_urls.append(href_absolute_url)
 
 def crawl_url():
-    print "*** NEW CRAWLING SESSION FOR URL: %s ***" % initial_url
+    print "\n* NEW CRAWLING SESSION FOR URL: %s *\n" % initial_url
 
     while len(urls_to_visit) > 0:
         current_url = urls_to_visit.pop(0)
-        current_url_parsed = urlparse.urlparse(current_url)        
-        html_data = None
-        met_mimetype_criteria = False
-        met_file_extension_criteria = False                                        
-        print "Starting to process url: ", current_url
-        print len(urls_to_visit)
-        # Look for a valid head response from the URL
-        head_response = requests.head(current_url)
-        if not head_response.status_code == requests.codes.ok:
-            print "Received an invalid head response from URL: ", current_url
-        else:
-            head_content_type = head_response.headers.get('content-type')
-            # If we found an HTML file, grab all the links
-            if 'text/html' in head_content_type:
-                get_response = requests.get(current_url)
-                if get_response.status_code == requests.codes.ok:
-                    html_data = get_response.text
-                    add_new_urls(current_url, html_data)
-            # Check if we should download files with this mimetype or extension
-            for mimetype in mimetypes_list:
-                if mimetype in head_content_type:
-                    met_mimetype_criteria = True
-            if not met_mimetype_criteria:
-                for file_extension in file_extensions_list:
-                    if file_extension in current_url: # This could be swapped for urlparse(current_url).path...[-1]
-                        met_file_extension_criteria = True                
-            # Check if we should download this file based on potential regex restrictions, only if it passes the mimetype or extension tests
-            if met_mimetype_criteria or met_file_extension_criteria:
-                if not using_regex_filters:
-                    download_file(current_url, html_data)
-                else:
-                    for regex_filter in regex_filters:
-                        if regex_filter.search(current_url):
-                            download_file(current_url, html_data)
-                            break                
-            print "Finished processing url: ", current_url
-                
+        try:
+            current_url_parsed = urlparse.urlparse(current_url)
+            html_data = None
+            met_mimetype_criteria = False
+            met_file_extension_criteria = False                                        
+            print "\nSTARTING TO PROCESS URL: %s\n" % current_url
+            print "Remaining URLs: %d" % len(urls_to_visit)
+            # Look for a valid head response from the URL
+            head_response = requests.head(current_url, timeout=request_timeout)
+            if not head_response.status_code == requests.codes.ok:
+                print "Received an invalid head response"
+            else:
+                head_content_type = head_response.headers.get('content-type')
+                # If we found an HTML file, grab all the links
+                if 'text/html' in head_content_type:
+                    get_response = requests.get(current_url, timeout=request_timeout)
+                    if get_response.status_code == requests.codes.ok:
+                        html_data = get_response.text
+                        encoding = get_response.encoding                    
+                        add_new_urls(current_url, html_data)
+                # Check if we should download files with this mimetype or extension
+                for mimetype in mimetypes_list:
+                    if mimetype in head_content_type:
+                        met_mimetype_criteria = True
+                if not met_mimetype_criteria:
+                    for file_extension in file_extensions_list:
+                        if file_extension in current_url: # This could be swapped for urlparse(current_url).path...[-1]
+                            met_file_extension_criteria = True                
+                # Check if we should download this file based on potential regex restrictions, only if it passes the mimetype or extension tests
+                if met_mimetype_criteria or met_file_extension_criteria:
+                    if not using_regex_filters:
+                        download_file(current_url, html_data, encoding)
+                    else:
+                        for regex_filter in regex_filters:
+                            if regex_filter.search(current_url):
+                                download_file(current_url, html_data, encoding)
+                                break
+            print "Files written: %d" % files_written
+            print "Finished processing URL"
+        except:
+            try:
+                traceback_info = '\n'.join(traceback.format_exception(*(sys.exc_info())))
+            except:
+                traceback_info = ''
+            print "*** ERROR PROCESSING: %s ***\nTraceback: %s\n" % ( current_url, traceback_info )
+            
 if __name__ == "__main__":
     output_dir = os.path.join( os.getcwd(), "output" )
     if not os.path.exists(output_dir):
@@ -113,5 +127,5 @@ if __name__ == "__main__":
             using_regex_filters = True
             regex_filters = [ re.compile(regex_filter) for regex_filter in regex_filters ]
         else:
-            using_regex_filters = False            
+            using_regex_filters = False
         crawl_url()
