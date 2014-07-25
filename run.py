0227e44bcd7d51f65a528b0e463add6d686131f7
Ryan McCormack <writepython@gmail.com>
2014-07-24 23:24:35 -0400

    Just dealing with paths and writing files to disk

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
-import os, errno
-#import urllib
-import urlparse
+import os, errno, string, urlparse, mimetypes
 import requests
-import mimetypes
 from bs4 import BeautifulSoup
 
 from config import urls_to_crawl, file_extensions_to_download
 
+fs_path_acceptabl_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._'
+
+fs_path_trans_table  = string.maketrans( fs_path_acceptabl_chars, '_' * len(fs_path_acceptabl_chars) )
+    
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -17,7 +18,7 @@ def mkdir_p(path):
 
 def download_file(current_url, data):
     """
-    Data will either be data from an HTML file or None for other file types
+    Data will either be data from an HTML file or None
     """
     print "Attempting to download file: ", current_url
     if not data:
@@ -28,9 +29,26 @@ def download_file(current_url, data):
             print "Could not get data from file: ", current_url
     if data:
         url_parsed = urlparse.urlparse(current_url)
-        netloc_dir = os.path.join(output_dir, url_parsed.netloc)
-        if not os.path.exists(netloc_dir):
-            mkdir_p(netloc_dir)
+        netloc = url_parsed.netloc
+        url_path = url_parsed.path.strip()
+        if url_path in ["/", "", ".", ".."]:
+            fs_path = netloc
+            with open('workfile', 'r') as f:
+            filename = "root.file"
+            fs_path = None
+        elif url_path.endswith("/"): # Still not catching the edge case where we have file /abc and then later directory /abc/
+            fs_path = url_path + "root.file"
+        else:
+            path = url_path.strip("/")
+            path = url_path.split("/")
+            path = [ p.translate(fs_path_trans_table) for p in path ]
+            path = "/".join(path)
+
+
+with open('workfile', 'r') as f:            
+        #netloc_dir = os.path.join(output_dir, url_parsed.netloc)
+        #if not os.path.exists(netloc_dir):
+        #mkdir_p(netloc_dir)
         url_path = url_parsed.path
         if url_path.endswith("/") or url_path == "":
             filename = "path_root.html"
@@ -49,8 +67,10 @@ def crawl_url():
 
     while len(urls_to_visit) > 0:
         current_url = urls_to_visit.pop(0)
+        current_url_parsed = urlparse.urlparse(current_url)        
         html_data = None
-        found_html = False        
+        met_mimetype_criteria = False
+        met_file_extension_criteria = False                                        
         print "Starting to process url: ", current_url
         print len(urls_to_visit)
         # Look for a valid head response from the URL
@@ -64,31 +84,24 @@ def crawl_url():
                 get_response = requests.get(current_url)
                 if get_response.status_code == requests.codes.ok:
                     html_data = get_response.text
-                    found_html = True
                     add_new_urls(current_url, html_data)
-            # Check if we should download files with this extension
-            proper_extension = False
-            url_path = urlparse.urlparse(current_url).path
-            for file_extension in file_extensions_to_download:
-                if file_extension in url_path:
-                    proper_extension = True
-                    break
-                # If we want to download all HTML files and found HTML at a non-standard extension or at "/"
-                elif file_extension == '.html' and found_html:
-                    proper_extension = True
-                    break
-                elif file_extension == '.htm' and found_html:
-                    proper_extension = True
-                    break
-            # Check if we should download this file based on regex restrictions, only if it passed the extension test
-            if proper_extension:
+            # Check if we should download files with this mimetype or extension
+            for mimetype in mimetypes:
+                if mimetype in head_content_type:
+                    met_mimetype_criteria = True
+            if not met_mimetype_criteria:
+                for file_extension in file_extensions:
+                    if file_extension in current_url: # This could be swapped for urlparse(current_url).path...[-1]
+                        met_file_extension_criteria = True                
+            # Check if we should download this file based on potential regex restrictions, only if it passes the mimetype or extension tests
+            if met_mimetype_criteria or met_file_extension_criteria:
                 if not using_regex_filters:
                     download_file(current_url, html_data)
                 else:
                     for regex_filter in regex_filters:
                         if regex_filter.search(current_url):
                             download_file(current_url, html_data)
-                            break
+                            break                
             print "Finished processing url: ", current_url
                 
 if __name__ == "__main__":
@@ -101,7 +114,10 @@ if __name__ == "__main__":
         urls_to_visit = [initial_url]
         all_urls = [initial_url]
         follow_link_containing = d["follow_links_containing"]
-        regex_filters = [ re.compile(regex_filter) for regex_filter in d["regex_filters"] ]
+        regex_filters = d.get("regex_filters")
         if regex_filters:
-            using_regex_filters = True                          
+            using_regex_filters = True
+            regex_filters = [ re.compile(regex_filter) for regex_filter in regex_filters ]
+        else:
+            using_regex_filters = False            
         crawl_url()
