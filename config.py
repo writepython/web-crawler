87fea0a9bacf258510cf9601d22c022c4b73e62f
Ryan McCormack <writepython@gmail.com>
2014-07-30 19:14:45 -0400

    Restored simple config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ request_timeout = 20
 
 urls_to_crawl = [
     {
-        "url": "http://www.sanwebe.com",
-        "follow_links_containing": "www.sanwebe.com",
+        "url": "http://madeinheights.com",
+        "follow_links_containing": "madeinheights.com",
+        "regex_filters": [ r"st.ry" ]
     },
 ]
