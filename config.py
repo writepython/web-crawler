bc640d4cc1ac4b5cfe3ec87253ec92a39ffe85c2
Ryan McCormack <writepython@gmail.com>
2016-09-03 14:49:52 -0400

    Insignificant changes to config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ request_delay = 0
 
 urls_to_crawl = [
     {
+		"url": "http://www.cuyoo.com/article-22417-1.html",
+		"follow_links_containing": "http://www.cuyoo.com/article-22417-1.html",
+		"ignore_query_strings": True,
+    },    
+    {
 		"url": "http://www.dalailama.com/webcasts/post/360-meeting-with-the-shia-and-sunni-communities-in-leh",
 		"follow_links_containing": "dalailama.com",
 		"ignore_query_strings": True,
     },    
-    {
-		"url": "http://www.cuyoo.com/article-22417-1.html",
-		"follow_links_containing": "http://www.cuyoo.com/article-22417-1.html",
-		"ignore_query_strings": True,
-    },
 ]
