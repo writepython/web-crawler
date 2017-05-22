426d4ba6c9536da6a0a3115c1877ea4c9cad8d31
Ryan McCormack <writepython@gmail.com>
2017-05-22 17:04:21 -0400

    Create add_musicbrainz_artists.py

---
+++ add_musicbrainz_artists.py
@@ -0,0 +1,14 @@
+## sudo apt-get install python-musicbrainzngs
+
+import csv, musicbrainzngs
+
+musicbrainzngs.set_useragent("Example music app", "0.1", "http://example.com/music")
+with open('/home/vagrant/a.csv', 'wb') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(["ID", "Name"])
+    f.flush()
+    for i in range(981):
+        result = musicbrainzngs.search_artists(country="US", limit=100, offset=i)
+        for artist in result['artist-list']:
+	        csv_writer.writerow([artist['id'], artist["name"]])
+	        f.flush()
