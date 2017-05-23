af15d67dee33e2da771ab571378e8ef397449915
Ryan McCormack <writepython@gmail.com>
2017-05-22 23:29:53 -0400

    Successfully retrieving all US MusicBrainz artists

--- add_musicbrainz_artists.py
+++ add_musicbrainz_artists.py
@@ -1,15 +1,20 @@
 ## sudo apt-get install python-musicbrainzngs
-## sed '/^$/d' file.txt | wc -l
+## sed '/^$/d' output/a.csv | wc -l
 
 import csv, musicbrainzngs
 
 musicbrainzngs.set_useragent("Example music app", "0.1", "http://example.com/music")
-with open('/home/vagrant/a.csv', 'wb') as f:
+x = []
+with open('output/a.csv', 'wb') as f:
     csv_writer = csv.writer(f)
     csv_writer.writerow(["ID", "Name"])
     f.flush()
+    offset = 0
     for i in range(981):
-        result = musicbrainzngs.search_artists(country="US", limit=100, offset=i)
+        result = musicbrainzngs.search_artists(country="US", limit=100, offset=offset)
         for artist in result['artist-list']:
-	        csv_writer.writerow([artist['id'], artist["name"].encode('utf-8')])
-	    f.flush()
+            csv_writer.writerow([artist['id'], artist["name"].encode('utf-8')])
+        print "Finished iteration %d" % i
+        offset += 100
+        if '9' in str(i):
+            f.flush()
