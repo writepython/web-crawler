f5e2ab169b0e09f4e40f58175016f0e71e877606
Ryan McCormack <writepython@gmail.com>
2017-05-30 15:28:55 -0400

    Many improvements in output and performance

--- update_contact_info.py
+++ update_contact_info.py
@@ -7,33 +7,36 @@ USAGE_MESSAGE = 'Usage: update_contact_info.py -i <input_file> -o <output_file>'
 REQUEST_HEADERS = { 'User-Agent': 'Mozilla/5.0' }
 EMAIL_REGEX = re.compile(r"[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]+")
 
+ignore_query_strings = False
+ignore_anchors = False
+site_urls_cutoff = 36
+current_row = 0
 contact_info_dict = {}
-ignore_query_strings = True
-ignore_anchors = True
-site_urls_cutoff = 50
+
+BLACKLISTED_ENDINGS = (
+    '.mp3', '.wav', '.m4a', '.3gp', '.ogg', '.flac', '.wma', '.aiff', '.m3u',
+    '.mp4', '.mov', '.m4v', '.wmv', 
+    '.jpg', '.jpeg', '.png', '.gif',
+    '.7z', '.zip', '.cals', '.tar', '.gz',
+    '.pdf',
+)
 
 def add_contact_info(seed_url, html):
+    email_addresses = []
     html = html.replace('&#064;', '@')
-    email_addresses = re.findall(EMAIL_REGEX, html)
+    potential_email_addresses = re.findall(EMAIL_REGEX, html)
+    if potential_email_addresses:
+        for potential_email in potential_email_addresses:
+            if not potential_email.lower().endswith(BLACKLISTED_ENDINGS):
+                email_addresses.append(potential_email)
     if email_addresses:
         contact_info_dict[seed_url]['email'] = list(set(contact_info_dict[seed_url]['email'] + email_addresses))
 
 def fits_url_blacklist(url):
-    BLACKLISTED_CONTAINS = ['wikipedia', 'youtube', 'facebook', 'twitter', 'last.fm', 'tumblr', 'myspace', 'instagram']
-    BLACKLISTED_ENDINGS = [
-        '.mp3', '.wav', '.m4a', '.3gp', '.ogg', '.flac', '.wma', '.aiff', '.m3u',
-        '.mp4', '.mov', '.m4v', '.wmv', 
-        '.jpg', '.jpeg', '.png', '.gif',
-        '.7z', '.zip', '.cals', '.tar', '.gz',
-        '.pdf',
-    ]
-    url_lowercase = url.lower()
-    for blacklisted_contain in BLACKLISTED_CONTAINS: 
-        if blacklisted_contain  in url_lowercase:    
-            return True
-    for blacklisted_ending in BLACKLISTED_ENDINGS: 
-        if url_lowercase.endswith(blacklisted_ending):    
-            return True        
+    # BLACKLISTED_CONTAINS = ['wikipedia', 'youtube', 'facebook', 'twitter', 'last.fm', 'tumblr', 'myspace', 'instagram']
+    if url.lower().endswith(BLACKLISTED_ENDINGS):
+        return True
+    
     return False
 
 def get_modified_seed_url(url):
@@ -51,13 +54,12 @@ def get_modified_seed_url(url):
     return url
 
 def add_new_urls(url, seed_url, page_source):
-    if len(all_urls) > site_urls_cutoff:
-        print "Reached site URLs cutoff for %s" % seed_url
-        return True
-    else:
-        print "Adding new URLs from page source of URL: %s" % url
+    print "Adding new URLs from page source of URL: %s" % url
     parsed_html = BeautifulSoup(page_source)
     for tag in parsed_html.findAll('a', href=True):
+        if len(all_urls) > site_urls_cutoff:
+            print "Reached site URLs cutoff for %s" % seed_url
+            return True
         href = tag['href'].strip() # Stripping handles <a href=" http...
         if ignore_anchors:
             anchor_index = href.find("#") 
@@ -70,17 +72,21 @@ def add_new_urls(url, seed_url, page_source):
                     href = href[:query_string_index]
             href_absolute_url = urlparse.urljoin(url, href)
             if href_absolute_url.startswith('http'): # We don't care about mailto:foo@bar.com etc.                
-                if fits_url_blacklist(href_absolute_url): # Ignore blacklisted
-                    continue
-                if contact_info_dict[seed_url]['seed_url_hostname'] in href_absolute_url or contact_info_dict[seed_url]['final_url_hostname'] in href_absolute_url: # Part of the same domain as the seed URL                               
+                href_absolute_url = get_modified_seed_url(href_absolute_url)
+                if href_absolute_url:
+                    #if contact_info_dict[seed_url]['seed_url_hostname'] in href_absolute_url or contact_info_dict[seed_url]['final_url_hostname'] in href_absolute_url: # Part of the same domain as the seed URL                               
                     if href_absolute_url not in all_urls:                
                         urls_to_visit.append(href_absolute_url)
                         all_urls.append(href_absolute_url)
         
-def crawl_url(seed_url):
+def crawl_url(seed_url, musicbrainz_id, musicbrainz_name):
     global errors_encountered
-    print "\n* NEW CRAWLING SESSION FOR URL: %s *\n" % seed_url
-    contact_info_dict[seed_url] = { 'seed_url_hostname': '', 'final_url': '', 'final_url_hostname': '', 'email': [], 'phone': [], 'twitter': [] }
+    global current_row
+    global contact_info_dict
+    current_row += 1
+    print "\n* (%d) NEW CRAWLING SESSION FOR URL: %s *\n" % (current_row, seed_url)
+    # contact_info_dict[seed_url] = { 'seed_url_hostname': '', 'final_url': '', 'final_url_hostname': '', 'email': [], 'phone': [], 'twitter': [] }
+    contact_info_dict[seed_url] = { 'seed_url_hostname': '', 'final_url': '', 'final_url_hostname': '', 'email': [] }
     is_seed_url = True
     
     while len(urls_to_visit) > 0:
@@ -108,19 +114,22 @@ def crawl_url(seed_url):
                             add_contact_info(seed_url, page_source)
                         if is_seed_url:
                             add_new_urls(final_url, seed_url, page_source)
-                        elif contact_info_dict[seed_url]['seed_url_hostname'] in final_url:
-                            add_new_urls(final_url, seed_url, page_source)
-                        elif contact_info_dict[seed_url]['final_url_hostname'] in final_url:
-                            add_new_urls(final_url, seed_url, page_source)                        
+                        # elif contact_info_dict[seed_url]['seed_url_hostname'] in final_url:
+                        #     add_new_urls(final_url, seed_url, page_source)
+                        # elif contact_info_dict[seed_url]['final_url_hostname'] in final_url:
+                        #     add_new_urls(final_url, seed_url, page_source)                        
 
             is_seed_url = False                                    
             global files_processed
             files_processed += 1
             print "Files Found: %d  Processed: %d  Remaining: %d  Operational Errors: %d" % ( len(all_urls), files_processed, len(urls_to_visit), errors_encountered )
-            print contact_info_dict
+            # print contact_info_dict
             if len(urls_to_visit) == 0:
-                csv_writer.writerow([ seed_url, contact_info_dict[seed_url]['seed_url_hostname'], contact_info_dict[seed_url]['final_url'], contact_info_dict[seed_url]['final_url_hostname'], ', '.join(contact_info_dict[seed_url]['email']) ])
-                f.flush()
+                for email in contact_info_dict[seed_url]['email']:
+                    csv_writer.writerow([ musicbrainz_id, musicbrainz_name, seed_url, contact_info_dict[seed_url]['seed_url_hostname'], contact_info_dict[seed_url]['final_url'], contact_info_dict[seed_url]['final_url_hostname'], email ])
+                contact_info_dict = {}
+                if files_processed % 2 == 0:
+                    f.flush()
         except:
             errors_encountered += 1
             try:
@@ -147,29 +156,31 @@ if __name__ == "__main__":
     if not input_file or not output_file:
         print USAGE_MESSAGE
         sys.exit(2)
-    with open(input_file) as f:
-        urls = f.readlines()
-    print "Found %d URLs" % len(urls)
 
     with open(output_file, 'wb') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow([ 'seed_url', 'seed_url_hostname', 'final_url', 'final_url_hostname', 'emails' ])
+        csv_writer.writerow([ 'Artist ID', 'Artist Name', 'seed_url', 'seed_url_hostname', 'final_url', 'final_url_hostname', 'email' ])
         f.flush()
-        for url in urls:
-            url = url.strip()
-            if not url:
-                continue
-            url = get_modified_seed_url(url)
-            if not url:
-                continue            
-            files_processed = 0
-            errors_encountered = 0
-            urls_to_visit = [url]
-            all_urls = [url]
+        with open(input_file, 'rb') as r:
+            musicbrainz_url_reader = csv.reader(r)
+            for reader_row in musicbrainz_url_reader:
 
-            start_time = datetime.datetime.now()
-            print "\nCurrent Time:  %s" % start_time
-            crawl_url(url)
-            end_time = datetime.datetime.now()
-            print contact_info_dict
-            print "\nStart:  %s\nFinish: %s\n" % (start_time, end_time)
+                    musicbrainz_id = reader_row[0]
+                    musicbrainz_name = reader_row[1]
+                    url = reader_row[2].strip()
+                    if not url:
+                        continue
+                    url = get_modified_seed_url(url)
+                    if not url:
+                        continue            
+                    files_processed = 0
+                    errors_encountered = 0
+                    urls_to_visit = [url]
+                    all_urls = [url]
+
+                    start_time = datetime.datetime.now()
+                    print "\nCurrent Time:  %s" % start_time
+                    crawl_url(url, musicbrainz_id, musicbrainz_name)
+                    end_time = datetime.datetime.now()
+                    print "\nStart:  %s\nFinish: %s\n" % (start_time, end_time)
+
